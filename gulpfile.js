c2f559ac3ba153e231adf5e58f1416c0818313db
Stephan Sum <sum@live.de>
2016-08-24 21:08:34 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('scss/*.scss') // to watch ALL: ./**/*.scss
+        .pipe(sass().on('error', sass.logError)) // .on('error', sass.logError)  to swallow error
+        .pipe(gulp.dest(function(f) {
+            return f.base;
+        }))
+});
+
+
+
+gulp.task('default', ['sass'], function() {
+    // dont use "./" in front of pathes, because otherwise gulp-watch isnt able to recognize newly or deleted files
+    gulp.watch('scss/*.scss', ['sass']);
+})
