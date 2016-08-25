c1480a354d828fcdbc33c6179a2b9f0848b8c0c1
Stephan Sum <sum@live.de>
2016-08-25 18:24:10 +0200

    save

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('scss/*.scss') // to watch ALL: ./**/*.scss
+    gulp.src('styles/*.scss') // to watch ALL: ./**/*.scss
         .pipe(sass().on('error', sass.logError)) // .on('error', sass.logError)  to swallow error
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
+        // use this function within gulp.dest() to place the output in the same directory:  function(f) { return f.base; }
 });
 
 
 
 gulp.task('default', ['sass'], function() {
     // dont use "./" in front of pathes, because otherwise gulp-watch isnt able to recognize newly or deleted files
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('styles/*.scss', ['sass']);
 })
+
+
+
+// AFTER MAKING CHANGES TO THE .GULPFILE RESTART THE GULP TASK !
